e7f34871d407fef36debdd86eceae018f5eea30a
SHIN-MINGYU <smg20004@naver.com>
2024-08-04 02:02:19 +0900

    ✨ feat : html에 맵 캔버스 추가하고, 실제 화면에 렌더링

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,16 +1,31 @@
 import PlayScreen from "./screens/Play";
+import MapScreen from "./screens/Map";
 import Player from "./models/player";
+import TestMap from "./models/maps";
 
 const canvas: HTMLCanvasElement = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+const mapCanvas: HTMLCanvasElement = document.querySelector("#mapCanvas");
+const mapCtx = mapCanvas.getContext("2d");
+
 canvas.width = 1920;
 canvas.height = 1080;
+
+mapCanvas.width = 1920;
+mapCanvas.height = 1080;
 
 const playScreen = PlayScreen.getInstance(ctx, canvas);
 const player = new Player(ctx, canvas);
 
+const mapScreen = MapScreen.getInstance(mapCtx, mapCanvas);
+
 playScreen.registerUpdateQueue("player", player);
 playScreen.registerDrawQueue("player", player);
+
+const map = new TestMap(mapCtx, mapCanvas, { x: 0, y: 0 });
+mapScreen.registerDrawQueue("testMap", map);
+
+mapScreen.drawOnce();
 
 window.requestAnimationFrame(playScreen.draw);
