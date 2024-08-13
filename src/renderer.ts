4da385b451c24fb9470cae7a37f5db1ae144bb2f
SHIN-MINGYU <smg20004@naver.com>
2024-08-13 10:14:34 +0900

    ✨ feat :  플레이 스크린에 로직 등록

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,13 +9,14 @@ canvas.width = 1920;
 canvas.height = 1080;
 
 const playScreen = PlayScreen.getInstance(ctx, canvas);
-const player = new Player(ctx, canvas, 100);
-
-playScreen.registerUpdateQueue("player", player);
-playScreen.registerDrawQueue("player", player);
 
 const map = new TestMap(ctx, canvas, 0, { x: 0, y: 0 });
 
 playScreen.registerDrawQueue("map", map);
+
+const player = new Player(ctx, canvas, 100, [map]);
+
+playScreen.registerUpdateQueue("player", player);
+playScreen.registerDrawQueue("player", player);
 
 window.requestAnimationFrame(playScreen.draw);
